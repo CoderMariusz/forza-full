00bb599b47b8342bd9ddc3540e3d22397f5fefab
Mariusz Krawczyk <przyslony@gmail.com>
2024-02-09 21:42:23 +0000

    coment merge table

--- lib/MargeTables.tsx
+++ lib/MargeTables.tsx
@@ -1,23 +1,23 @@
-import { Product } from '@/store/Products';
+// import { Product } from '@/store/Products';
 
-function mergeTables(products: any, labels: any) {
-  return products.map((product: Product) => {
-    const result = {
-      aCode: product.aCode,
-      $id: product.$id,
-      packetInBox: product.packetInBox,
-      labels: product.labels?.map((label) => {
-        const foundLabel = labels.find((l: any) => l.code === label.code);
+// function mergeTables(products: any, labels: any) {
+//   return products.map((product: Product) => {
+//     const result = {
+//       aCode: product.aCode,
+//       $id: product.$id,
+//       packetInBox: product.packetInBox,
+//       labels: product.labels?.map((label) => {
+//         const foundLabel = labels.find((l: any) => l.code === label.code);
 
-        return {
-          code: label.code,
-          quantities: foundLabel ? foundLabel.quantities : 0,
-          $id: foundLabel ? foundLabel.$id : null // Here we are adding id to the label as well.
-        };
-      })
-    };
-    return result;
-  });
-}
+//         return {
+//           code: label.code,
+//           quantities: foundLabel ? foundLabel.quantities : 0,
+//           $id: foundLabel ? foundLabel.$id : null // Here we are adding id to the label as well.
+//         };
+//       })
+//     };
+//     return result;
+//   });
+// }
 
-export default mergeTables;
+// export default mergeTables;
